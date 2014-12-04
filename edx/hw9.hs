b3e2df61d2af2fd744a1e32ecb1e77c18f5cb5a8
taptap <alexey.e.egorov@gmail.com>
2014-12-04 15:40:08 +0300

    finish hw9

--- edx/hw9.hs
+++ edx/hw9.hs
@@ -9,11 +9,11 @@ data Nat = Zero
 nat2int :: Nat -> Int
 nat2int = head . m
   where m Zero = [0]
-        m (Succ n) = [sum [x | x <- (1 : m n)]]
+        m (Succ n) = [Data.List.sum [x | x <- (1 : m n)]]
 
-int2nat :: Int -> Nat
-int2nat (n+1) = Succ (int2nat n)
-int2nat 0 = Zero
+-- int2nat :: Int -> Nat
+-- int2nat (n+1) = Succ (int2nat n)
+-- int2nat 0 = Zero
 
 add :: Nat -> Nat -> Nat
 add n (Succ m) = Succ (add m n)
@@ -22,3 +22,21 @@ add n Zero = n
 mult :: Nat -> Nat -> Nat
 mult m Zero = Zero
 mult m (Succ n) = add m (mult m n)
+
+data Tree = Leaf Integer
+          | Node Tree Integer Tree
+
+occurs m (Leaf n) = m == n
+occurs m (Node l n r)
+  = case compare n m of
+  LT -> occurs m l
+  EQ -> True
+  GT -> occurs m r
+
+class Monoid a where
+  mempty :: a
+  (<>) :: a -> a -> a
+
+instance Monoid [a] where
+  mempty = []
+  (<>) = (++)
